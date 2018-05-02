3d715d244328b2fc434347e539cf0e4162d0e5d6
hachinobu <hachinobu@gmail.com>
2018-05-02 14:50:46 +0900

    Initial commit.

---
+++ CoordinatorQiitaClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoordinatorQiitaClient
+//
+//  Created by Takahiro Nishinobu on 2018/05/02.
+//  Copyright © 2018年 hachinobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
